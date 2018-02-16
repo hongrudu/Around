dc403dac340843fcb001556c1c6dc9d34eb98b31
Hongru Du <hongrudu@gmail.com>
2018-02-15 22:48:37 -0500

    new ip

--- service/main.go
+++ service/main.go
@@ -19,7 +19,7 @@ const(
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.30.2:9200"
+	ES_URL = "http://35.196.224.188:9200"
 )
 
 type Location struct {
